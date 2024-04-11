8e255dd6ea99e6ccb545ca4cb24ab42911c7add5
wanglei <dkzx-wanglei@ajiot.net>
2024-04-11 17:28:40 +0800

    1、fix：add config model

--- model.go
+++ model.go
@@ -1,13 +1,16 @@
 package gm_client
 
 type MqttClient struct {
-	Broker   string `json:"broker"`
-	Port     int    `json:"port"`
-	User     string `json:"user"`
-	Pass     string `json:"pass"`
-	ClientID string `json:"clientID,optional"`
-	Qos      int    `json:"qos,optional"`
-	CrtPem   string `json:"crtPem"`
-	KeyPem   string `json:"keyPem"`
-	Ca       string `json:"ca,optional"`
+	Broker      string `json:"broker"`
+	Port        int    `json:"port"`
+	User        string `json:"user"`
+	Pass        string `json:"pass"`
+	ClientID    string `json:"clientID,optional"`
+	Qos         int    `json:"qos,optional"`
+	CrtPem      string `json:"crtPem"`
+	KeyPem      string `json:"keyPem"`
+	Ca          string `json:"ca,optional"`
+	Topic       string `json:"topic,optional"`
+	StationId   string `json:"stationId,optional"`
+	StationName string `json:"stationName,optional"`
 }
